60ea51d7e501359b7570ff27ccb5dc9e1dbc9312
proencaf <83221947+proencaf@users.noreply.github.com>
2022-03-18 12:23:43 +0000

    fetching movie data working

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css'
+import { useEffect, useState } from 'react'
+import Movie from './components/Movie'
+
+function App() {
+  const [popular, setPopular] = useState([])
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
+  const fetchMovies = async () => {
+    const data = await fetch(
+      'https://api.themoviedb.org/3/movie/popular?api_key=ce570fde957f596d6f2922064e204782'
+    )
+    const movies = await data.json()
+    setPopular(movies.results)
+  }
+
+  return (
+    <div className='App'>
+      <div className='popular-movies'>
+        {popular.map((movie) => {
+          return <Movie key={movie.id} movie={movie} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
